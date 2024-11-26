f2275473d0a1bffe24450f47c88837ac51c2bc48
Chan Jeong <chjeong@c2r7s4.42gyeongsan.kr>
2024-11-26 18:38:08 +0900

    1

--- includes/env.h
+++ includes/env.h
@@ -12,17 +12,25 @@
 
 #ifndef ENV_H
 # define ENV_H
+# include "libft.h"
+
+typedef struct s_envp_node
+{
+	char				*key;
+	char				*value;
+}	t_envp_node;
 
 // envp.c
-char	***get_envp(void);
-void	set_envp(char **new_envp);
-void	free_envp(void);
-int		get_envp_len(void);
-void	init_envp(char **envp);
+t_list		**get_envp(void);
+t_envp_node	*make_envp_node(const char *name, const char *value);
+void		free_envp(void);
+int			get_equal_idx(char *str);
+
 
 // env.c
 char	*ft_getenv(const char *name);
-int		ft_setenv(const char *name, const char *value);
-int		ft_unsetenv(const char *name);
+int	ft_setenv(const char *name, const char *value);
+int	ft_unsetenv(const char *name);
+
 
 #endif
--- srcs/env/envp.c
+++ srcs/env/envp.c
@@ -14,6 +14,95 @@
 #include "libft.h"
 #include <stdlib.h>
 
+
+t_list	**get_envp(void)
+{
+	static t_list	*envp;
+
+	return (&envp);
+}
+
+void	set_envp(t_list	*new_envp)
+{
+	free_envp();
+	*get_envp() = new_envp;
+}
+
+void	free_envp(void)
+{
+	t_list		*envp;
+	t_list		*tmp;
+	t_envp_node	*node;
+
+	envp = *get_envp();
+	while (envp != NULL)
+	{
+		node = (t_envp_node *)envp->content;
+		free(node->key);
+		free(node->value);
+		free(envp->content);
+		tmp = envp->next;
+		free(envp);
+		envp = tmp;
+	}
+}
+
+void	sset_envp(t_envp_node *node, char *env)
+{
+	int		idx;
+
+	idx = get_equal_idx(env);
+	node->key = ft_substr(env, 0, idx);
+	node->value = ft_substr(&env[idx + 1], 0, ft_strlen(&env[idx + 1]));
+}
+
+int	get_equal_idx(char *str)
+{
+	int	idx;
+
+	idx = 0;
+	while (str[idx])
+	{
+		if (str[idx] == '=')
+			return idx;
+		idx++;
+	}
+	return -1;
+}
+
+t_envp_node	*make_envp_node(const char *name, const char *value)
+{
+	t_envp_node	*node;
+
+	node = (t_envp_node *)malloc(sizeof(t_envp_node));
+	if (node == NULL)
+		return (NULL);
+	node->key = ft_strdup(name);
+	node->value = ft_strdup(value);
+	return (node);
+}
+
+void	init_envp(char **envp)
+{
+	t_list	*head;
+	char	*name;
+	char	*value;
+	int		idx;
+
+	head = *get_envp();
+	while (*envp != NULL)
+	{
+		idx = get_equal_idx(*envp);
+		name = ft_substr(*envp, 0, idx);
+		value = ft_substr(&(*envp[idx + 1]), 0, ft_strlen(&(*envp[idx + 1])));
+		ft_lstadd_back(&head, ft_lstnew(make_envp_node(name, value)));
+		envp++;
+	}
+	set_envp(head);
+}
+
+
+/*
 char	***get_envp(void)
 {
 	static char	**envp;
@@ -73,3 +162,4 @@ void	init_envp(char **envp)
 	}
 	(*envpp)[len] = NULL;
 }
+*/
